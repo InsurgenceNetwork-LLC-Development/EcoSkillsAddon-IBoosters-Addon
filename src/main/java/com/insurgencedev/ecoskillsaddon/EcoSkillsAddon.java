945bf46550508b91ee0caa91f8222d24e533e6ee
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-28 00:06:02 -0500

    api jar update

--- src/main/java/com/insurgencedev/ecoskillsaddon/EcoSkillsAddon.java
+++ src/main/java/com/insurgencedev/ecoskillsaddon/EcoSkillsAddon.java
@@ -1,16 +1,16 @@
 package com.insurgencedev.ecoskillsaddon;
 
 import com.insurgencedev.ecoskillsaddon.listeners.EcoSkillsEventListener;
-import org.bukkit.Bukkit;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
+import org.insurgencedev.insurgenceboosters.libs.fo.Common;
 
 @IBoostersAddon(name = "EcoSkillsAddon", version = "1.0.1", author = "InsurgenceDev", description = "EcoSkills Support")
 public class EcoSkillsAddon extends InsurgenceBoostersAddon {
 
     @Override
-    public void onAddonReloadablesStart() {
-        if (Bukkit.getPluginManager().isPluginEnabled("EcoSkills")) {
+    public void onAddonReloadAblesStart() {
+        if (Common.doesPluginExist("EcoSkills")) {
             registerEvent(new EcoSkillsEventListener());
         }
     }
--- src/main/java/com/insurgencedev/ecoskillsaddon/listeners/EcoSkillsEventListener.java
+++ src/main/java/com/insurgencedev/ecoskillsaddon/listeners/EcoSkillsEventListener.java
@@ -4,8 +4,7 @@ import com.willfp.ecoskills.api.event.PlayerSkillXPGainEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.insurgencedev.insurgenceboosters.api.IBoosterAPI;
-import org.insurgencedev.insurgenceboosters.models.booster.GlobalBoosterManager;
-import org.insurgencedev.insurgenceboosters.settings.IBoostersPlayerCache;
+import org.insurgencedev.insurgenceboosters.data.BoosterFindResult;
 
 public final class EcoSkillsEventListener implements Listener {
 
@@ -13,19 +12,19 @@ public final class EcoSkillsEventListener implements Listener {
     private void onGain(PlayerSkillXPGainEvent event) {
         final String TYPE = "Skills";
         final String NAMESPACE = "ECO_SKILLS";
-        double totalMulti = 1;
+        final double[] totalMulti = {1};
 
-        IBoostersPlayerCache.BoosterFindResult pResult = IBoosterAPI.getCache(event.getPlayer()).findActiveBooster(TYPE, NAMESPACE);
-        if (pResult instanceof IBoostersPlayerCache.BoosterFindResult.Success boosterResult) {
-            totalMulti += boosterResult.getBooster().getMultiplier();
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
+        if (pResult instanceof BoosterFindResult.Success boosterResult) {
+            totalMulti[0] += boosterResult.getBoosterData().getMultiplier();
         }
 
-        GlobalBoosterManager.BoosterFindResult gResult = IBoosterAPI.getGlobalBoosterManager().findBooster(TYPE, NAMESPACE);
-        if (gResult instanceof GlobalBoosterManager.BoosterFindResult.Success boosterResult) {
-            totalMulti += boosterResult.getBooster().getMultiplier();
-        }
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
+            totalMulti[0] += globalBooster.getMultiplier();
+            return null;
+        }, () -> null);
 
-        event.setGainedXP(calculateAmount(event.getGainedXP(), totalMulti));
+        event.setGainedXP(calculateAmount(event.getGainedXP(), totalMulti[0]));
     }
 
     private long calculateAmount(double amount, double multi) {
